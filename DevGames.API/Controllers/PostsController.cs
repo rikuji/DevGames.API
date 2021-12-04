505faf62a14c4915807cdd574526ee6dd4c0a9cc
rikuji <lucasbezerra1701@gmail.com>
2021-12-04 11:54:35 -0300

    Add project files.

---
+++ DevGames.API/Controllers/PostsController.cs
@@ -0,0 +1,81 @@
+﻿using DevGames.API.Entities;
+using DevGames.API.Models;
+using DevGames.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevGames.API.Controllers
+{
+    [Route("api/boards/{id}/posts")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly DevGamesContext context;
+
+        public PostsController(DevGamesContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll(int id)
+        {
+            var board = context.Boards.SingleOrDefault(b => b.Id == id);
+
+            if (board == null)
+                return NotFound();
+
+            return Ok(board.Posts);
+        }
+
+        [HttpGet("{postId}")]
+        public IActionResult GetById(int id, int postId)
+        {
+            var board = context.Boards.SingleOrDefault(b => b.Id == id);
+
+            if (board == null)
+                return NotFound();
+
+            var post = board.Posts.SingleOrDefault(p => p.Id == postId);
+
+            if (post == null)
+                return NotFound();
+
+            return Ok(post);
+        }
+
+        [HttpPost]
+        public IActionResult Post(int id, AddPostInputModel model)
+        {
+            var board = context.Boards.SingleOrDefault(b => b.Id == id);
+
+            if (board == null)
+                return NotFound();
+
+            var post = new Post(model.Id, model.Title, model.Description);
+
+            board.Posts.Add(post);
+
+            return CreatedAtAction(nameof(GetById), new { id = id, postId = post.Id }, model);
+        }
+
+        [HttpPost("{postId}/comments")]
+        public IActionResult PostComment(int id, int postId, AddCommentInputModel model)
+        {
+            var board = context.Boards.SingleOrDefault(b => b.Id == id);
+
+            if (board == null)
+                return NotFound();
+
+            var post = board.Posts.SingleOrDefault(p => p.Id == postId);
+
+            if (post == null)
+                return NotFound();
+
+            var comment = new Comment(model.Title, model.Description, model.User);
+
+            post.AddComment(comment);
+
+            return NoContent();
+        }
+    }
+}
---
+++ DevGames.API/Persistence/DevGamesContext.cs
@@ -0,0 +1,13 @@
+﻿using DevGames.API.Entities;
+
+namespace DevGames.API.Persistence
+{
+    public class DevGamesContext
+    {
+        public DevGamesContext()
+        {
+            Boards = new List<Board>();
+        }
+        public List<Board> Boards { get; private set; }
+    }
+}
